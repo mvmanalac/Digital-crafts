17772a92295bd15ffc8083e7a1f40765c586e7fa
Marcus Manalac <marcusvmanalac@gmail.com>
2023-04-29 10:24:42 -0700

    add postman items under auth-demo-jan2023

--- AUTH-DEMO-JAN2023/server.js
+++ AUTH-DEMO-JAN2023/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const { checkAuth } = require('./middleware');
 
 const server = express();
+server.use(express.json());
+server.use(checkAuth)
+
 
 server.get('/heartbeat', (req, res) => {
     res.json({
         "is": "working"
     })
+});
+
+server.post('/auth/login', (req, res) => {
+    res.send(`${req.body.username}, ${req.body.password}`);
 });
 
 server.listen(8080, () => {
