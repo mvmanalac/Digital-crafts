3e5df5ab373064fa3830b8ca094d061fad05d7a2
Marcus Manalac <marcusvmanalac@gmail.com>
2023-05-02 17:59:05 -0700

    updating repo 05.02

--- AUTH-DEMO-JAN2023/server.js
+++ AUTH-DEMO-JAN2023/server.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const { checkAuth } = require('./middleware');
+const pgp = require('pg-promise')();
+
 
 const server = express();
 server.use(express.json());
 server.use(checkAuth)
 
+const cn = {
+    host: 'localhost',
+    portL 5432,
+    database: 'users',
+    user: 'postgres',
+    password: 'password',
+    allowExitOnIdle: true
+}
 
 server.get('/heartbeat', (req, res) => {
     res.json({
         "is": "working"
     })
 });
+
+server.get("/test", async (req, res) => {
+    const user = await test();
+    res.json(user)
+});
+
+async function test() {
+    const user = await debugger.any('SELECT * FROM users', [true]);
+    return user
+};
 
 server.post('/auth/login', (req, res) => {
     res.send(`${req.body.username}, ${req.body.password}`);
